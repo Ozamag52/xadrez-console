68b646912ddb0a86295c7361094629f025c5cda0
Gustavo Peixoto <henriquegustavo2010@gmail.com>
2020-04-28 12:34:15 -0300

    Comecando a colocar pecas no tabuleiro

--- xadrez-console/tabuleiro/Tabuleiro.cs
+++ xadrez-console/tabuleiro/Tabuleiro.cs
@@ -4,22 +4,29 @@ namespace tabuleiro
 {
     class Tabuleiro
     {
-        public int linhas { get; set; }
-        public int colunas { get; set; }
-        private Peca[,] pecas;
+        public int linhas { get; set; } // Linhas do tabuleiro 
+        public int colunas { get; set; } // Colunas do tabuleiro 
+        private Peca[,] pecas; // Matriz de pecas do tipo Peca que so pode ser acessado e alterado dentro da classe Tabuleiro
 
 
-        public Tabuleiro(int linhas, int colunas)
+        public Tabuleiro(int linhas, int colunas) // Construtor do tabuleiro 
         {
             this.linhas = linhas;
             this.colunas = colunas;
-            pecas = new Peca[linhas, colunas];
+            pecas = new Peca[linhas, colunas]; // Instancia a matriz de pecas com a mesma dimensão do tabuleiro 
         }
 
-        public Peca peca(int linha, int coluna)
+        public Peca peca(int linha, int coluna) // Encapsulamento do atributo Matriz de Peças com a função de get 
         {
 
             return pecas[linha, coluna];
+        }
+
+        public void colocarPeca(Peca p, Posicao pos) // Função responsavel por adicionar a matriz de peças uma peça 
+        {
+            pecas[pos.linha, pos.coluna] = p;
+            p.posicao = pos;
+
         }
     }
 }
---
+++ xadrez-console/xadrez/Rei.cs
@@ -0,0 +1,19 @@
+﻿using tabuleiro;
+
+namespace xadrez
+{
+    class Rei : Peca // A classe Rei e uma classe que herda os atributos e metodos da classe generica Peca
+    {
+
+
+        public Rei(Tabuleiro tab, Cor cor) : base(tab , cor)
+        {
+        }
+
+        public override string ToString()
+        {
+            return "R"; 
+        }
+
+    }
+}

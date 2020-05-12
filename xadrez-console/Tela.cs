fa7135612077c16b75bb5e8371699054e8d30616
Gustavo Peixoto <henriquegustavo2010@gmail.com>
2020-05-12 19:43:16 -0300

    Controlando as pecas capturadas

--- xadrez-console/Tela.cs
+++ xadrez-console/Tela.cs
@@ -1,10 +1,50 @@
 ﻿using System;
+using System.Collections.Generic;
 using tabuleiro;
 using xadrez;
 namespace xadrez_console
 {
     class Tela
     {
+
+        public static void imprimirPartida(PartidaDeXadrez partida)
+        {
+            imprimirTabuleiro(partida.tab);
+            Console.WriteLine();
+            imprimirPecasCapturadas(partida);
+            Console.WriteLine("Turno: " + partida.turno);
+            Console.WriteLine("Aguardando jogada: " + partida.jogadorAtual);
+        }
+
+        public static void imprimirPecasCapturadas(PartidaDeXadrez partida)
+        {
+            Console.WriteLine("Peças capturadas:");
+            Console.Write("Brancas:");
+            imprimirConjunto(partida.pecasCapturadas(Cor.Branca));
+            Console.Write("Pretas:");
+            ConsoleColor aux = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            imprimirConjunto(partida.pecasCapturadas(Cor.Preta));
+            Console.ForegroundColor = aux;
+            Console.WriteLine();
+        }
+
+
+
+        public static void imprimirConjunto(HashSet<Peca> conjuunto)
+        {
+            Console.Write("[");
+            foreach(Peca x in conjuunto)
+            {
+                Console.Write(x + " ");
+            }
+            Console.Write("]");
+            Console.WriteLine();
+        }
+
+
+
+
 
         public static void imprimirTabuleiro(Tabuleiro tab)
         {
